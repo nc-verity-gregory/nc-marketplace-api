80bad9c7d81ba9b1e77b02e47c838fb799f3d50e
Haz Mallion <harriet.mallion@northcoders.com>
2024-06-26 12:17:12 +0100

    adds rate-limiting based on users IP address

--- __tests__/limiter.test.js
+++ __tests__/limiter.test.js
@@ -1,10 +1,10 @@
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
 
-const app = require('../app');
-const request = require('supertest');
-const db = require('../db/connection');
-const seed = require('../db/seeds/seed');
-const testData = require('../db/data/test/index');
+const app = require("../app");
+const request = require("supertest");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test/index");
 
 beforeEach(async () => {
   await seed(testData);
@@ -14,19 +14,19 @@ afterAll(async () => {
   await db.end();
 });
 
-describe('Rate Limiter', () => {
-  it('should respond with an error if more than 30 requests are made within 15 minutes', async () => {
+describe("Rate Limiter", () => {
+  it("should respond with an error if more than 25 requests are made within 3 minutes", async () => {
     const promiseToMakeApiRequest = [];
-    for (let index = 0; index < 30; index++) {
-      promiseToMakeApiRequest.push(request(app).get('/api/users'));
+    for (let index = 0; index < 25; index++) {
+      promiseToMakeApiRequest.push(request(app).get("/api/users"));
     }
     await Promise.all(promiseToMakeApiRequest);
     await request(app)
-      .get('/api/users')
+      .get("/api/users")
       .expect(429)
       .then(({ body: { msg } }) => {
         expect(msg).toBe(
-          'Received too many requests - Check your code for infinite loops!'
+          "Received too many requests - check your code for infinite loops. You can send requests again in 3 minutes."
         );
       });
   });
